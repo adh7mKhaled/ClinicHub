43f3d8fe118174d0c55dfd901b6a83ea64a9d839
Adham khaled <158836492+adh7mKhaled@users.noreply.github.com>
2025-06-24 23:45:50 +0300

    Add TinyMce For Patient Form

--- ClinicHub/Core/ViewModels/PatientFormViewModel.cs
+++ ClinicHub/Core/ViewModels/PatientFormViewModel.cs
@@ -5,15 +5,24 @@ namespace ClinicHub.Core.ViewModels;
 public class PatientFormViewModel
 {
 	public int Id { get; set; }
+
+	[Display(Name = "First Name")]
 	public string FirstName { get; set; } = null!;
+
+	[Display(Name = "Last Name")]
 	public string LastName { get; set; } = null!;
 
 	[AssertThat("DateOfBirth < Today()", ErrorMessage = Errors.NotAllowDates)]
+	[Display(Name = "Date Of Birth")]
 	public DateTime DateOfBirth { get; set; }
 
 	public Gender Gender { get; set; }
+
+	[Display(Name = "Marital Status")]
 	public MaritalStatus MaritalStatus { get; set; }
 	public string? Notes { get; set; }
+
+	[Display(Name = "Phone Number")]
 	public string PhoneNumber { get; set; } = null!;
 	public string Address { get; set; } = null!;
 
---
+++ ClinicHub/wwwroot/js/patient.js
@@ -0,0 +1,4 @@
+﻿tinymce.init({
+    selector: '.js-tinymce',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+});
