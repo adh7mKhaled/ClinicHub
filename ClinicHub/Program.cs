8d1b02ef960d1cf59a8961511fce9bf2a7bc4ec3
Adham khaled <158836492+adh7mKhaled@users.noreply.github.com>
2025-07-05 18:22:38 +0300

    Add Identity Pages

--- ClinicHub/Program.cs
+++ ClinicHub/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddApplicationServices(builder);
 
 var app = builder.Build();
